75b6ba9ef6082a173e39d825bca2b0524ecfc293
Ezra Shaw <ezrashawdev@gmail.com>
2024-05-15 17:42:28 +1200

    chore: finish documenting `src/snake.rs` + more cleanup

--- src/snake.rs
+++ src/snake.rs
@@ -44,29 +44,37 @@ pub fn game_main(mut canvas: Canvas) -> io::Result<Option<usize>> {
     // the number of game cells, divided by size of each value (64 bits). note also that division
     // rounds down, so we have to add another u64 (which will only be partly filled).
     let mut bitboard = vec![0u64; canvas.w() as usize * canvas.h() as usize / 64 + 1];
+    // write the snake's head into the bitboard early so that the fruit generation can't place a
+    // fruit on the spawn position
+    set_bb(&mut bitboard, &canvas, head, true);
     // initialize the snake's length to the starting length
     let mut len = STARTING_LENGTH;
     // initialize the fruits by choosing random locations on the canvas,
-    // FIXME: we haven't yet init'ed the bitboard with the snake's initial position, so we could
-    // spawn a fruit and then override it by spawning the snake.
     // FIXME: don't unwrap the result of the `gen_fruit` function
     let mut fruits: [Coord; FOOD_COUNT] =
         array::from_fn(|_| gen_fruit(&mut rng, &mut canvas, &mut bitboard).unwrap());
 
     loop {
+        // put the head into the tail, and mark it as occupied on the bitboard
         tail.push_back(head);
         set_bb(&mut bitboard, &canvas, head, true);
+
+        // if the tail is longer than the snake's length, trim it
         if tail.len() > len {
             let coord = tail.pop_front().unwrap();
-            canvas.clear_pixel(coord)?;
 
+            canvas.clear_pixel(coord)?;
             set_bb(&mut bitboard, &canvas, coord, false);
         }
 
+        // draw the snake's head onto the canvas
         canvas.draw_pixel(head, Color::Lime)?;
 
+        // sleep for 140ms, but wait for keys at the same time
         let time = Instant::now();
         if let Some(key) = canvas.poll_key(STEP_MS)? {
+            // map keys to directions, keeping in mind that the snake can't turn immediately back
+            // on itself
             match key {
                 Key::Up if direction != Direction::Down => direction = Direction::Up,
                 Key::Down if direction != Direction::Up => direction = Direction::Down,
@@ -82,6 +90,7 @@ pub fn game_main(mut canvas: Canvas) -> io::Result<Option<usize>> {
             sleep(Duration::from_millis(t));
         }
 
+        // actually move the snake's head position, checking to see if we have hit a wall
         let old_pos = head;
         match direction {
             Direction::Up if head.y > 0 => head.y -= 1,
@@ -91,19 +100,23 @@ pub fn game_main(mut canvas: Canvas) -> io::Result<Option<usize>> {
             _ => break,
         }
 
+        // TODO: remove `fruits` array and merge this conditional with the next
+        // check if we have hit our own tail
         if get_bb(&bitboard, &canvas, head) && !fruits.contains(&head) {
-            // make sure to reset the head position back to where it was,
-            // otherwise the animation mucks up
+            // make sure to reset the head position back to where it was, otherwise the animation
+            // mucks up
             head = old_pos;
             break;
         }
 
+        // draw the previous head position as the tail colour
         canvas.draw_pixel(old_pos, Color::Green)?;
 
+        // check if we have encountered a fruit
         if let Some(fruit_idx) = fruits.iter().position(|&f| f == head) {
             len += 1;
-            // shouldn't remove fruit from bitboard because we ate it and will
-            // "digest" it (normal snake code will remove it)
+            // shouldn't remove fruit from bitboard because we ate it and will "digest" it (normal
+            // snake code will remove it)
             fruits[fruit_idx] = gen_fruit(&mut rng, &mut canvas, &mut bitboard)?;
         }
     }
@@ -117,25 +130,50 @@ pub fn game_main(mut canvas: Canvas) -> io::Result<Option<usize>> {
     canvas.draw_pixel(head, Color::BrightRed)?;
     sleep(Duration::from_millis(500));
 
+    // return the score, calculated as the difference between the initial and current length
     Ok(Some(len - STARTING_LENGTH))
 }
 
+/// Creates a fruit at a random position on the canvas, accounting for other fruits and the snake.
+///
+/// The naive approach to generating fruits is simple:
+/// 1. Choose a random location.
+/// 2. If it is not free, then repeat 1.
+/// 3. Place the fruit on the canvas.
+///
+/// This approach has a significant issue. When there are few free squares left (that is, when the
+/// snake is very long), a possibly infinite number of random numbers might be generated. In other
+/// words, there is no fixed upper time bound for this algorithm.
+///
+/// Instead, a better algorithm is used that is O(n) on the size of the canvas:
+/// 1. Calculate the number of free squares.
+/// 2. Generate a random number between 0 and the number of free squares.
+/// 3. Map the generated index onto the canvas (we iterate over the whole canvas, and only
+/// increment on free squares).
+/// 4. Place the fruit on the canvas.
 fn gen_fruit(rng: &mut File, canvas: &mut Canvas, bitboard: &mut [u64]) -> io::Result<Coord> {
-    let mut idx = [0u8; 8];
-    rng.read_exact(&mut idx)?;
+    // read eight bytes (a u64) into a buffer
+    let mut rand = [0u8; 8];
+    rng.read_exact(&mut rand)?;
     let rand = usize::from_le_bytes(idx);
 
+    // calculate how many filled and free squares there are
     let filled = bitboard.iter().map(|x| x.count_ones()).sum::<u32>() as usize;
-    let target_idx = rand % ((canvas.w() as usize * canvas.h() as usize) - filled + 1);
+    let free = (canvas.w() as usize * canvas.h() as usize) - filled + 1;
+    // calculate our target square based on the number of free squares and our random number
+    let target_idx = rand % free;
 
     let mut idx = 0;
     let (mut fx, mut fy) = (u16::MAX, u16::MAX);
     // for each xy point on the canvas...
     'outer: for y in 0..canvas.h() {
         for x in 0..canvas.w() {
+            // we only increment our index on free squares
             if !get_bb(bitboard, canvas, Coord { x, y }) {
                 idx += 1;
             }
+
+            // if we have made it to the target index, then exit
             if idx >= target_idx {
                 fx = x;
                 fy = y;
@@ -144,17 +182,25 @@ fn gen_fruit(rng: &mut File, canvas: &mut Canvas, bitboard: &mut [u64]) -> io::R
         }
     }
 
+    // sanity check to that we did manage to find a position for the fruit. note that this is
+    // reached when the player "wins" (fills up whole canvas with the snake).
+    // TODO: gracefully handle the win condition
     assert_ne!(fx, u16::MAX);
 
+    // mark our new fruit's location on the bitboard and draw it to the screen
     let coord = Coord { x: fx, y: fy };
     set_bb(bitboard, canvas, coord, true);
     canvas.draw_pixel(coord, Color::BrightYellow)?;
+
     Ok(coord)
 }
 
+/// Mark a coordinate on the bitboard as either occupied or unoccupied.
 fn set_bb(bitboard: &mut [u64], canvas: &Canvas, coord: Coord, value: bool) {
+    // turn the 2d coordinate into a flat index
     // TODO: inline `as_idx`
     let idx = coord.as_idx(canvas);
+    // use magic bitwise operators to set/unset
     if value {
         bitboard[idx / 64] |= 0b1 << (idx % 64);
     } else {
@@ -162,12 +208,16 @@ fn set_bb(bitboard: &mut [u64], canvas: &Canvas, coord: Coord, value: bool) {
     }
 }
 
+/// Check whether a coordinate on the bitboard is occupied or unoccupied.
 fn get_bb(bitboard: &[u64], canvas: &Canvas, coord: Coord) -> bool {
+    // turn the 2d coordinate into a flat index
     // TODO: inline `as_idx`
     let idx = coord.as_idx(canvas);
+    // use magic bitwise operators to check if the bit is marked as occupied
     bitboard[idx / 64] & (0b1 << (idx % 64)) != 0
 }
 
+/// Enumeration representing the four possible directions that the snake can be moving in.
 #[derive(PartialEq, Eq)]
 enum Direction {
     Up,
