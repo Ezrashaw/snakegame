f104a4bbea051182fd9bcf28b98e9fb489f76090
Ezra Shaw <ezrashawdev@gmail.com>
2024-05-31 15:42:02 +1200

    add gzip helper to psf-util

--- crates/psf-util/src/main.rs
+++ crates/psf-util/src/main.rs
@@ -1,6 +1,25 @@
+use std::{
+    fs,
+    io::Write,
+    process::{Command, Stdio},
+};
+
 use psf_util::read_psf;
 
 fn main() {
-    let psf = read_psf(include_bytes!("../../default8x16.psfu"));
+    let bytes = ungzip(&fs::read("/usr/share/kbd/consolefonts/default8x16.psfu.gz").unwrap());
+    let psf = read_psf(&bytes);
     psf.print_table();
+}
+
+fn ungzip(bytes: &[u8]) -> Vec<u8> {
+    let mut cmd = Command::new("gzip")
+        .arg("-d")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    cmd.stdin.as_mut().unwrap().write_all(bytes).unwrap();
+    cmd.wait_with_output().unwrap().stdout
 }
