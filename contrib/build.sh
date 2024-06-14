2dd8a228d4f8454e749fb194477cd7666b09b783
Ezra Shaw <ezrashawdev@gmail.com>
2024-06-14 19:27:48 +1200

    Revert "make the build script more conservative"
    This reverts commit 796366c14f6b518b5d4bdf093e6cd6970a70ba25. I am
    fairly OK with this, but we'll have to see whether it causes illegal
    hardware instructions again *sigh*

--- contrib/build.sh
+++ contrib/build.sh
@@ -10,15 +10,15 @@ cp contrib/setup.sh contrib/snake/setup.sh
 chmod +x contrib/snake/setup.sh
 
 # Build main snake package.
-# cargo clean
-cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-musl --release
-cp target/x86_64-unknown-linux-musl/release/snake contrib/snake/snake
+cargo clean
+RUSTFLAGS="-Zlocation-detail=none -Ctarget-cpu=native -Clink-args=-Wl,-build-id=none,--no-eh-frame-hdr -Crelocation-model=static" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-musl --release
+objcopy -R .eh_frame -R .got.plt -R .comment target/x86_64-unknown-linux-musl/release/snake contrib/snake/snake
 
 # Build snake server package.
 cd crates/snake-server
-cargo build --target x86_64-unknown-linux-musl --release
+RUSTFLAGS="-Zlocation-detail=none -Ctarget-cpu=native -Clink-args=-Wl,-build-id=none,--no-eh-frame-hdr -Crelocation-model=static" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-unknown-linux-musl --release
 cd ../..
-cp target/x86_64-unknown-linux-musl/release/snake-server contrib/snake/snake-server
+objcopy -R .eh_frame -R .got.plt -R .comment target/x86_64-unknown-linux-musl/release/snake-server contrib/snake/snake-server
 
 # Create patched 16x32 consolefont.
 cd crates/psf-util
